15d10a943b40902ef1ed0bc64b528b56b18d87ed
Aleksey <aljasones@yandex.ru>
2019-10-22 00:42:17 +0300

    add objects

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -10,9 +10,15 @@ public class Main {
     public static void main(String[] args) {
         ScoreService scoreService = new ScoreService(new ScoreRepository());
 
-        scoreService.addScore(new TV(0, "LG", 10000, 32, "Black", true, true));
+        scoreService.addScore(new TV(0, "LG", 10000, 32, "Black", false, true));
+        scoreService.addScore(new TV(0, "Sony", 15000, 54, "Black", true, true));
+        scoreService.addScore(new TV(0, "BBK", 5000, 24, "Black", false, false));
         scoreService.addScore(new Notebook(0,"Pentium", 20000, "Intel", "Pentium4", 4, 250, true));
+        scoreService.addScore(new Notebook(0,"AMDGame", 30000, "AMD", "Razen", 16, 500, true));
+        scoreService.addScore(new Notebook(0,"Mac", 50000, "Apple", "Xeon", 32, 750, true));
         scoreService.addScore(new Phone(0, "Galaxy S8", 30000, "Samsung", "Snapdragon", 4,128, 5.5,true));
+        scoreService.addScore(new Phone(0, "IphoneX", 90000, "Apple", "A7", 6,128, 5.2,true));
+        scoreService.addScore(new Phone(0, "Honor", 40000, "Huawei", "Kirin", 4,64, 6.5,true));
 
 
 
